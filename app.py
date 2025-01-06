602409db3ef6a6931daace69f05dda61b5c2f242
enzomtp <enzojore@live.fr>
2025-01-06 12:43:58 +0100

    navbar fix

--- app.py
+++ app.py
@@ -10,19 +10,12 @@ custom_route_names = {
     "/admin": "Admin"
 }
 
-def get_routes():
-    routes = []
-    for rule in app.url_map.iter_rules():
-        if rule.endpoint != 'static':
-            routes.append(rule.rule)
-    return routes
-
 @app.context_processor
 def inject_routes():
-    return dict(routes=get_routes(), custom_route_names=custom_route_names)
+    return dict(custom_route_names=custom_route_names)
 
 app.add_url_rule("/", "index", index)
-app.add_url_rule("/", "admin", admin)
+app.add_url_rule("/admin", "admin", admin)
 
 if __name__ == "__main__":
     debug_mode = os.getenv('FLASK_ENV') == 'development'
