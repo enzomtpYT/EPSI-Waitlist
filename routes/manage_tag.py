b4252c9cf3159302b7a81a91e72966e8434b8ff1
FunsizedWrath <151151740+FunsizedWrath@users.noreply.github.com>
2025-01-09 15:47:37 +0100

    tags can be deleted

--- routes/manage_tag.py
+++ routes/manage_tag.py
@@ -27,33 +27,6 @@ def manage_tag():
         flash("Erreur lors de la récupération des tags", "danger")
     return render_template('manage_tag.html', tags=tags)
 
-# @manage_tag_bp.route("/admin/manage_tag")
-# def manage_tag():
-#     tags, error = database.get_all_tags()
-#     if error:
-#         flash("Erreur lors de la récupération des tags", "danger")
-#     return render_template('manage_tag.html', tags=tags)
-
-# @manage_tag_bp.route("/admin/manage_tag/<int:id_tag>/edit", methods=['GET', 'POST'])
-# def edit_tag(id_tag):
-#     tag, error = database.get_tag(id_tag)
-
-#     if request.method == 'POST':
-#         name = request.form['tag_name']
-#         error = None
-
-#         if not name:
-#             error = 'Le nom est obligatoire.'
-
-#         if error is None:
-#             error = database.edit_tag(name, id_tag)
-#             if error is None:
-#                 flash("Tag mis à jour avec succès!", "success")
-#                 return redirect(url_for('manage_tag.manage_tag'))
-#             else:
-#                 flash(f"Erreur lors de la mise à jour du tag: {error}", "danger")
-#     return render_template('edit_tag.html', tag=tag)
-
 @manage_tag_bp.route("/admin/manage_tag/<int:id_tag>/delete", methods=['POST'])
 def delete_tag(id_tag):
     error = database.delete_tag(id_tag)
