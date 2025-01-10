55d0f1d002fd47efd3c55768d88dd5f86bce41a2
FunsizedWrath <151151740+FunsizedWrath@users.noreply.github.com>
2025-01-10 16:28:03 +0100

    updated create_candidate

--- routes/create_candidate.py
+++ routes/create_candidate.py
@@ -37,26 +37,3 @@ def create_candidate():
         tags = []
 
     return render_template('create_candidate.html', tags=tags)
-
-    #     if error is None:
-    #         error = database.create_candidate(lastname, name, email)
-    #         if error:
-    #             flash(f"Erreur lors de la création du candidat: {error}", "danger")
-    #             return redirect(url_for('create_candidate.create_candidate'))
-    #         added_candidate, error = database.get_last_added_candidate()
-    #         if error:
-    #             flash(f"Erreur lors de la récupération du candidat: {error}", "danger")
-    #             return redirect(url_for('create_candidate.create_candidate'))
-    #         candidate_id = added_candidate['id_candidate']
-    #         for tag_id in tags:
-    #             error = database.add_tag_to_candidate(candidate_id, tag_id)
-    #             if error:
-    #                 flash(f"Erreur lors de l'ajout du tag: {error}", "danger")
-    #                 return redirect(url_for('create_candidate.create_candidate'))
-    #         flash("Candidat créé avec succès!", "success")
-    #         return redirect(url_for('create_candidate.create_candidate'))
-    #     else:
-    #         flash(f"Erreur lors de la création du candidat: {error}", "danger")
-    #         return redirect(url_for('create_candidate.create_candidate'))
-
-    # return render_template('create_candidate.html', tags=possible_tags)
