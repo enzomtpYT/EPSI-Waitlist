4a2e8adfa6593227f551cf6e19b601fb2561c20c
enzomtp <enzojore@live.fr>
2025-01-13 10:37:05 +0100

    multiple listes

--- routes/liste.py
+++ routes/liste.py
@@ -4,28 +4,27 @@ import time, json
 
 liste_bp = Blueprint('liste', __name__)
 
-def get_data():
-    todayevent, error = database.get_today_events()
-    event, error = database.get_event(todayevent)
+def get_data(id=None, error=None):
+    event, error = database.get_event(id)
     if error:
         message = error
         candid = None
         inter = None
     else:
-        candid, error = database.get_event_candidates(todayevent)
+        candid, error = database.get_event_candidates(id)
         if not candid:
             message = "Aucun candidats trouvés"
         else:
             message = None
 
-        inter, error = database.get_event_participant(todayevent)
+        inter, error = database.get_event_participant(id)
         if not inter:
             message = "Aucun intervenants trouvés"
 
     list = {}
 
     for interv in inter:
-        all, error = database.get_event_interview_candidate(todayevent, interv['id_participant'])
+        all, error = database.get_event_interview_candidate(id, interv['id_participant'])
         if interv['name_participant'] not in list:
             list[interv['name_participant']] = []
         for candid in all:
@@ -48,7 +47,15 @@ def refresh():
 
 @liste_bp.route("/liste")
 def liste():
-    d, mess = get_data()
+    today, error = database.get_today_events()
+    d, mess = get_data(today, error)
+    if mess:
+        flash(mess)
+    return render_template('liste.html', datas=d)
+
+@liste_bp.route("/liste/<int:id>")
+def liste_id(id):
+    d, mess = get_data(id)
     if mess:
         flash(mess)
     return render_template('liste.html', datas=d)
