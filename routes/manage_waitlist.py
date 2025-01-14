f876512491b82862fa07aa2c4c3a6f3039ffb9ae
enzomtp <enzojore@live.fr>
2025-01-14 11:35:46 +0100

    Check if already added

--- routes/manage_waitlist.py
+++ routes/manage_waitlist.py
@@ -18,17 +18,20 @@ def manage_waitlist(id_event):
         existing_interviews[participant['id_participant']] = []
         for row in b:
             existing_interviews[participant['id_participant']].append(row['id_candidate'])
-        print(existing_interviews)
+    print(f'------EXISTING------\n{existing_interviews}\n------EXISTING------')
 
     if request.method == 'POST':
         form_data = request.form.to_dict(flat=False)
         error = None
         
-        print(form_data)
+        print(f'------FORM DATA------\n{form_data}\n------FORM DATA------')
 
         for id_participant, all_candidates in form_data.items():
             for id_candidate in all_candidates:
-                error = database.create_interview(id_event, id_participant, id_candidate)
+                if int(id_candidate) in existing_interviews[int(id_participant)]:
+                    continue
+                else:
+                    error = database.create_interview(id_event, id_participant, id_candidate)
                 if error is not None:
                     break
             if error is not None:
