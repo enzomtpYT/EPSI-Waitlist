169eb6831d76dd1038cd4ddc84a185045ecf9b46
enzomtp <enzojore@live.fr>
2025-01-15 10:51:13 +0100

    hotfix

--- routes/api_router.py
+++ routes/api_router.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
 from lib import api
 
-api_bp = Blueprint('event', __name__)
+api_bp = Blueprint('api', __name__)
 
-@api_bp.route('/api/get', methods=['GET'])
+@api_bp.route('/api/get_event_participants', methods=['GET'])
 def manage_event_participants_api(id_event):
     datas, error = api.get_event_participants(id_event)
     return jsonify(datas)
