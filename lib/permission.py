cf42f7c887f8d07273a6ee80ce6d0cef02c496f4
enzomtp <enzojore@live.fr>
2025-01-21 10:50:37 +0100

    added permission checking for dashboards

--- lib/permission.py
+++ lib/permission.py
@@ -3,20 +3,22 @@ from lib import auth
 
 checks = {
     '/admin': 'admin.acess',
-    '/admin/manage_candidate': 'dashboard.view.candidate',
-    '/admin/manage_participant': 'dashboard.view.participant',
-    '/admin/manage_event': 'dashboard.view.events',
-    '/admin/manage_tag': 'dashboard.view.tags',
-    '/admin/manage_employee': 'dashboard.view.office',
-    '/admin/create_candidate': 'dashboard.create.candidate',
-    '/admin/create_participant': 'dashboard.create.participant',
-    '/admin/create_event': 'dashboard.create.event',
-    '/admin/create_tag': 'dashboard.create.tag',
-    '/admin/create_employee': 'dashboard.create.office',
-    '/admin/manage_candidate/candidate': 'dashboard.create.candidate',
-    '/admin/manage_participant/participant': 'dashboard.create.participant',
-    '/admin/manage_event/event': 'dashboard.create.event',
-    '/admin/manage_employee/employee': 'dashboard.create.office'
+    '/admin/manage_candidate': 'admin.dashboard.view.candidate',
+    '/admin/manage_participant': 'admin.dashboard.view.participant',
+    '/admin/manage_event': 'admin.dashboard.view.events',
+    '/admin/manage_tag': 'admin.dashboard.view.tags',
+    '/admin/manage_employee': 'admin.dashboard.view.office',
+    '/admin/create_candidate': 'admin.dashboard.create.candidate',
+    '/admin/create_participant': 'admin.dashboard.create.participant',
+    '/admin/create_event': 'admin.dashboard.create.event',
+    '/admin/create_tag': 'admin.dashboard.create.tag',
+    '/admin/create_employee': 'admin.dashboard.create.office',
+    '/admin/manage_candidate/candidate': 'admin.dashboard.create.candidate',
+    '/admin/manage_participant/participant': 'admin.dashboard.create.participant',
+    '/admin/manage_event/event': 'admin.dashboard.create.event',
+    '/admin/manage_employee/employee': 'admin.dashboard.create.office',
+    '/participant/dashboard': 'participant.dashboard',
+    '/candidate/dashboard': 'candidate.dashboard',
     
 }
 
@@ -25,7 +27,9 @@ def checkroutes(session):
         if request.path.startswith(route):
             print(f'Checking if user has permission {perm} for route {route}')
             if 'token' not in session:
+                print('No token in session')
                 return redirect(url_for('auth.login'))
             if not auth.check_permission(session['token'], perm):
                 flash("You don't have permission to access this page.")
                 return redirect('/')
+            print('User has permission')
