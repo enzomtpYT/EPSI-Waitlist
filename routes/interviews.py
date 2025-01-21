3e97bdaf9e11c5e743c08ba9c4c3ca63f4b57bc2
enzomtp <enzojore@live.fr>
2025-01-21 09:43:09 +0100

    editing feedback works

--- routes/interviews.py
+++ routes/interviews.py
@@ -3,24 +3,36 @@ from lib import database, api
 
 interviews_bp = Blueprint('interviews', __name__)
 
-@interviews_bp.route("/interviews")
+@interviews_bp.route("/interviews", methods=['GET', 'POST'])
 def interviews():
     if not 'token' in session:
         return redirect(url_for('auth.login'))
-    events, error = api.api_interviews(session)
+    session_token = session['token']
+    events, error = api.api_interviews(session_token)
+    if error:
+        flash('Error fetching interviews', 'danger')
+        return redirect(url_for('dashboard.dashboard'))
+    
+    # check if it is candidate or participant via session his session token
+    type = database.auth_get_user_type(session_token)
+    
     if request.method == 'POST':
         feedback = request.form.get('feedback')
-        id_interview = request.form.get('id_interview')
-        # check if it is candidate or participant via session his session token
+        id_interview = int(request.form.get('id_interview'))
         # check if interview already exists with feedback
-        for event in events:
+        for event in events.values():
             for interview in event['interviews']:
                 if interview['id_interview'] == id_interview:
-                    if interview['feedback']:
-                        flash("Feedback already exists for this interview", "danger")
-                        return redirect(url_for('interviews.interviews'))
+                    error = database.update_feedback(interview['id_interview'], feedback, type)
+                    if error:
+                        flash('Error updating feedback', 'danger')
+                    else:
+                        flash('Feedback updated', "success")
+                    return redirect(url_for('interviews.interviews'))
     return render_template('FeedbackInterviews.html', events=events)
 
 @interviews_bp.route("/api/interviews")
 def api_interviews():
-    return api.api_interviews(session)
+    if 'token' not in session:
+        return {"error": "Unauthorized"}, 401
+    return api.api_interviews(session_token = session['token'])
