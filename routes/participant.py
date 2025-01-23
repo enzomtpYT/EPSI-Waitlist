fd5f04718a1c60ca723396879b8f794f071aa884
enzomtp <enzojore@live.fr>
2025-01-23 12:12:37 +0100

    allow password change for participant

--- routes/participant.py
+++ routes/participant.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from lib import database
+from lib import database, auth
 import random, string
 
 participant_bp = Blueprint('participant', __name__)
@@ -18,6 +18,7 @@ def edit_participant(id_participant):
     if request.method == 'POST':
         name = request.form['participant_name']
         email = request.form['participant_email']
+        password = request.form['password']
         tags = request.form.getlist('tags')
         error = None
 
@@ -28,6 +29,8 @@ def edit_participant(id_participant):
 
         if error is None:
             error = database.edit_participant(name, email, id_participant)
+            if password:
+                error = auth.update_participant(id_participant, password)
             if error is None:
                 flash("Participant mis à jour avec succès!", "success")
                 return redirect(url_for('participant.edit_participant', id_participant=id_participant))
@@ -35,9 +38,7 @@ def edit_participant(id_participant):
                 flash(f"Erreur lors de la mise à jour du participant: {error}", "danger")
                 return redirect(url_for('participant.edit_participant', id_participant=id_participant))
 
-    # Generate random password min 8 characters max 16 characters with at least one uppercase letter, one lowercase letter, one number and one special character
-    password = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation.replace("'", "\\'")) for i in range(random.randint(8, 16)))
-    return render_template('participant.html', participant=participant, tags=tags, interviews=interviews, participant_tags=participant_tags, rpassword=password)
+    return render_template('participant.html', participant=participant, tags=tags, interviews=interviews, participant_tags=participant_tags)
 
 @participant_bp.route("/admin/manage_participant/participant/<int:id_participant>/add_tag_participant", methods=['POST'])
 def add_tag_participant(id_participant):
