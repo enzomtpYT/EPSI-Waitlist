6ccf03b1be2e352ed3e6dd79dfa7a7dac1fbb5b8
enzomtp <enzojore@live.fr>
2025-01-28 11:51:40 +0100

    hotfix employee couldn't change password

--- routes/employee.py
+++ routes/employee.py
@@ -57,6 +57,8 @@ def edit_employee(id_employee):
                 error = auth.update_user(password, username)
             error = database.edit_employee(lastname, name, email, id_employee)
             error = database.update_username_with_id(employee['id_user'], username)
+            if password:
+                error = auth.update_user(password, username)
             if error is None:
                 # Update the employee's role
                 error = database.update_user_role(employee['id_user'], role)
