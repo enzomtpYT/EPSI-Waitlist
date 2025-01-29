5b771725f56a392e2048806b025c6798cca055ba
enzomtp <enzojore@live.fr>
2025-01-29 12:20:01 +0100

    Added markdown support with links prevention

--- sock.py
+++ sock.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_misaka import Misaka
 import dotenv, os
 
 app = Flask(__name__)
 dotenv.load_dotenv()
 app.secret_key = os.getenv('FLASK_KEY')
 socketio = SocketIO(app)
+Misaka(app, no_intra_emphasis=True)
---
+++ static/scripts/base.js
@@ -0,0 +1,33 @@
+function updateLinks() {
+    const markdownLinks = document.querySelectorAll('.markdown a');
+    markdownLinks.forEach(link => {
+        link.onclick = function(event) {
+            if (!window.confirm('Vous allez être rediriger vers "' + link.href + '".\n\nVoulez-vous continuer ?')) {
+                event.preventDefault();
+            }
+        };
+    });
+}
+
+function observeMarkdownLinks() {
+    const targetNodes = document.querySelectorAll('.markdown');
+    const config = { childList: true, subtree: true };
+
+    const callback = function(mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                console.log('A child node has been added or removed.');
+                updateLinks();
+            }
+        }
+    };
+
+    const observer = new MutationObserver(callback);
+    targetNodes.forEach(node => observer.observe(node, config));
+}
+
+updateLinks();
+
+// Call the function to start observing
+observeMarkdownLinks();
+
