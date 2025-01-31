88a1aaba80f2f7557d195bbc74013d8f9e161aa9
enzomtp <enzojore@live.fr>
2025-01-31 09:20:27 +0100

    added generatepass to library

--- static/scripts/library.js
+++ static/scripts/library.js
@@ -27,3 +27,32 @@ function popup(category, message) {
         popup.remove();
     }, 6000);
 }
+
+function generatepass() {
+    // Generate random password min 12 characters max 24 characters with at least one uppercase letter, one lowercase letter, one number and one special character
+    var password = '';
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '0123456789';
+    var special = '!@#$%^&*()_+~`|}{[]\\:;?><,./-=';
+    var all = uppercase + lowercase + numbers + special;
+    var length = Math.floor(Math.random() * (24 - 8 + 1)) + 12;
+    for (var i = 0; i < length; i++) {
+        password += all.charAt(Math.floor(Math.random() * all.length));
+    }
+    return password;
+}
+
+// Toggle password visibility
+function showpass() {
+    var icon = document.getElementById('showpass');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+};
