40445fa776cb51f39bcfc66aa341b695ac3a0fa1
enzomtp <enzojore@live.fr>
2025-02-17 09:55:14 +0100

    failsafe ig

--- src/lib/api.py
+++ src/lib/api.py
@@ -374,6 +374,8 @@ def get_list(id, forced=False):
                                 interviews[part1][0], interviews[part1][r] = interviews[part1][r], interviews[part1][0]
                             hadswitch = True
             n += 1
+            if n > 10:
+                break
         for interview in interviews.values():
             for i, candidate in enumerate(interview):
                 candidate['position'] = i
