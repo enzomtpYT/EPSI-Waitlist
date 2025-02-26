8626f1462aafd1cf9c3faf3bc96cdfd4a29814ca
enzomtp <enzojore@live.fr>
2025-02-26 12:05:05 +0100

    Fix admin dashboard

--- src/routes/index.py
+++ src/routes/index.py
@@ -8,8 +8,10 @@ def index():
     if 'token' not in session:
         return redirect(url_for('auth.login'))
     session_token = session['token']
-    user_role = database.get_user_role_with_token(session_token)
-    print(f"User role: {user_role}")
+    user_role, error = database.get_user_role_with_token(session_token)
+    if error:
+        session.clear()
+        return redirect(url_for('auth.login'))
     if user_role == 'candidate' or user_role == 'participant':
         return redirect(url_for('dashboard.dashboard'))
     elif user_role in ['employee', 'admin', 'superadmin']:
