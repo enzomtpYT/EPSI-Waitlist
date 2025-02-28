76235dcde590ade87930baab25d7e72ece56c6d2
enzomtp <enzojore@live.fr>
2025-02-28 17:06:52 +0100

    uh??

--- src/sock.py
+++ src/sock.py
@@ -29,9 +29,7 @@ Misaka(app, no_intra_emphasis=True)
 if os.name == 'nt':
     async_mode = 'threading'
 else:
-    async_mode = 'eventlet'
-
-
+    async_mode = 'flask_socketio.GeventWebSocketServer'
 socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True, ping_timeout=30, ping_interval=10)
 @socketio.on('connect')
 def test_connect():
