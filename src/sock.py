8e8207b195774c12d52551363ac4e7d27ea628f4
enzomtp <enzojore@live.fr>
2025-02-28 16:52:26 +0100

    apparently this can fix?

--- src/sock.py
+++ src/sock.py
@@ -30,6 +30,9 @@ if os.name == 'nt':
     async_mode = 'threading'
 else:
     async_mode = 'eventlet'
+    import eventlet
+    eventlet.monkey_patch()
+
 
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode=async_mode, logger=True, engineio_logger=True)
 @socketio.on('connect')
